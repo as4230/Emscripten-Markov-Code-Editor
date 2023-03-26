ca1dbd7b134856857d8f0bf193def54a6278992d
as4230 <amscott9@ncsu.edu>
2023-03-26 19:52:12 -0400

    Remove data folder from main

--- data_python/20.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isValid(self, s):
-        brackets_stack, lefts, rights = [], ("(", "[", "{"), (")", "]", "}") 
-        for char in s:
-            if char in lefts: 
-                brackets_stack.append(char)
-            elif not brackets_stack or lefts.index(brackets_stack.pop()) != rights.index(char): 
-                return False
-        return not brackets_stack 
